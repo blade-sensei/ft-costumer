ee3338131bbb3e0fe111502dd52506f1fd0c824b
Coyla <jcoyla123@gmail.com>
2020-02-23 19:33:44 +0100

    test: add file test

---
+++ test.js
@@ -0,0 +1,12 @@
+const request = require('./index');
+
+const urls = [
+    'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json',
+    'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json',
+    'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json'
+    ];
+
+request.multiFetchUrl(urls).then(results => {
+    console.log(results)
+})
+
